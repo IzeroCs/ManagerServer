6a11fda92835b119718cfa9bfff3d3b6f032c443
IzeroCs <izero.cs@gmail.com>
2017-05-29 01:12:17 +0700

    First upload

---
+++ check_update.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo json_encode([
+        'version'         => '3.5.0 beta',
+        'change_msg'      => null,
+        'build_last'      => time(),
+        'compress_method' => 'zip',
+        'data_update'     => null
+    ]);
+
+?>
